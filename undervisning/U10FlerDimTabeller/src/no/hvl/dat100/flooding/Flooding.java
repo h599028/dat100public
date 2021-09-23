004241fa47495b6f218adcae57cfd472bd209b33
h599028 <599028@stud.hvl.no>
2021-09-23 19:26:56 +0200

    Oppgave B2 - oversvømmelse fullført

--- undervisning/U10FlerDimTabeller/src/no/hvl/dat100/flooding/Flooding.java
+++ undervisning/U10FlerDimTabeller/src/no/hvl/dat100/flooding/Flooding.java
@@ -1,19 +1,19 @@
 package no.hvl.dat100.flooding;
 
-import easygraphics.EasyGraphics;
+import easygraphics.*;
 
 public class Flooding extends EasyGraphics {
 
 	// nesten dekket hvis mindre en fra havoverflaten
 	int NESTEN_DEKKET = 1;
-	
+
 	int RADIUS = 20; // radius for cirkler som skal tegnes
-	int MARGIN = 50; // venstre/hÃ¸yre margin i tegneomrÃ¥det
+	int MARGIN = 50; // venstre/høyre margin i tegneområdet
 
 	// terreng hogde
-	int[][] terreng = { 
-			{ 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 }, 
-			{ 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 },
+	int[][] terreng = { //
+			{ 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 }, //
+			{ 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 }, //
 			{ 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 } };
 
 	public static void main(String[] args) {
@@ -25,29 +25,35 @@ public class Flooding extends EasyGraphics {
 		makeWindow("FLOODING", 800, 300);
 
 		visualiser();
+
 	}
 
 	public void visualiser() {
 
-		System.out.println("Angi havhÃ¸yde i tegnevinduet ...");
-		int hav = Integer.parseInt(getText("havhÃ¸yde"));
+		System.out.println("Angi havhøyde i tegnevinduet ...");
+		int hav = Integer.parseInt(getText("havhøyde"));
 
 		while (hav >= 0) {
 
-			// TODO - START
+			for (int i = 0; i < terreng.length; i++) {
 
-			// setColor(0,0,255); // blÃ¥ - vann
+				for (int j = 0; j < terreng[i].length; j++) {
 
-			// setColor(222,184,135); // lysebrun - tÃ¸rt
+					if (terreng[i][j] <= hav)
+						setColor(0, 0, 255);
+					else if (terreng[i][j] <= hav + 1)
+						setColor(255, 140, 0);
+					else
+						setColor(222, 184, 135);
 
-			// setColor(255,140,0); // oransje - sÃ¸le
+					fillCircle(MARGIN + 2 * j * RADIUS, MARGIN + 2 * i * RADIUS, RADIUS);
+				}
 
-			// fillCircle(MARGIN + * 2 * RADIUS,MARGIN + * 2* RADIUS,RADIUS);
+			}
 
-			// TODO - SLUTT
-
-			System.out.println("Angi havhÃ¸yde i tegnevinduet ...");
-			hav = Integer.parseInt(getText("havhÃ¸yde"));
+			System.out.println("Angi havhøyde i tegnevinduet ...");
+			hav = Integer.parseInt(getText("havhøyde"));
+			
 		}
 	}
 }
